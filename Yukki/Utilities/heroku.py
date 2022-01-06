97cbd241929850ffe0fcc5280bcff3bf3a930db1
Sorrybabe <86021848+Sorrybabe@users.noreply.github.com>
2022-01-07 04:47:51 +0530

    Update heroku.py

--- Yukki/Utilities/heroku.py
+++ Yukki/Utilities/heroku.py
@@ -10,15 +10,5 @@ async def user_input(input):
        return str(input.split(maxsplit=1)[1].strip())
     return ''
 
-useragent = (
-    "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/80.0.3987.149 Mobile Safari/537.36"
-)
 
-headers = {
-    "User-Agent": useragent,
-    "Authorization": f"Bearer {HEROKU_API_KEY}",
-    "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-}
     
